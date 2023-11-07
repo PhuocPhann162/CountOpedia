e4df81fc5185893b93d9920ca6dd52858eeda32a
Phuoc Phan <21520405@gm.uit.edu.vn>
2023-11-07 21:10:08 +0700

    done small counting game

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -10,63 +10,98 @@ class Counter extends React.Component {
     this.handleReset = this.handleReset.bind(this);
     this.handleRandomPlay = this.handleRandomPlay.bind(this);
     this.handleGameStatus = this.handleGameStatus.bind(this);
+    this.displayButton = this.displayButton.bind(this);
     this.state = {
       count: 0,
+      gameStatus: "",
+      lastPlay: "",
     };
   }
 
   handleAttack = () => {
     this.setState((previousState) => {
       let newCount = previousState.count + Math.round(Math.random() * 10);
+      this.handleGameStatus(newCount);
       return {
         count: newCount,
+        lastPlay: "Attack",
       };
     });
-  }
+  };
 
   handleDefence = () => {
     this.setState((previousState) => {
       let newCount = previousState.count - Math.round(Math.random() * 10);
+      this.handleGameStatus(newCount);
       return {
         count: newCount,
+        lastPlay: "Defence",
       };
     });
-  }
+  };
 
   handleRandomPlay = () => {
     let playMode = Math.round(Math.random());
     if (playMode === 0) {
       this.handleAttack();
+      this.handleGameStatus(this.state.count);
+      this.setState(() => {
+        return {
+          lastPlay: "Attack",
+        };
+      });
     } else {
       this.handleDefence();
+      this.handleGameStatus(this.state.count);
+      this.setState(() => {
+        return {
+          lastPlay: "Defence",
+        };
+      });
     }
-  }
+  };
 
   handleReset = () => {
     this.setState(() => {
       return {
         count: 0,
-      }
-    })
-  }
+      };
+    });
+  };
 
-  handleGameStatus = () => {
-    let cnt = this.state.count;
-    if(cnt > 10) {
-      return "You win!";
+  handleGameStatus = (cnt) => {
+    if (cnt >= 10) {
+      this.setState(() => {
+        return {
+          gameStatus: "You Win!",
+        };
+      });
     }
-    else {
-      return "In play process!";
+    if (cnt <= -10) {
+      this.setState(() => {
+        return {
+          gameStatus: "You lose!",
+        };
+      });
+      return false;
     }
-  }
+  };
+
+  displayButton = () => {
+    if (this.state.count >= 10 || this.state.count <= -10) {
+      return { display: "none" }; 
+    } else {
+      return {}; 
+    }
+  };
 
   render() {
     return (
       <div className="row text-white text-center">
         <h1>Game Score: {this.state.count}</h1>
         <p>You win at +10 points and lose at -10 points</p>
-        <p>Last Play: </p>
-        <h3>Game Status : {this.handleGameStatus}</h3>
+        <p>Last Play: {this.state.lastPlay}</p>
+        <h3>Game Status : {this.state.gameStatus}</h3>
         <div className="col-6 col-md-3 offset-md-3">
           <img
             alt="attack"
@@ -94,9 +129,20 @@ class Counter extends React.Component {
           />
         </div>
         <div className="col-12 col-md-4 offset-md-4">
-          <button onClick={this.handleRandomPlay} className="btn btn-secondary w-100 mt-3">Random Play</button>
+          <button
+            style={this.displayButton()}
+            onClick={this.handleRandomPlay}
+            className="btn btn-secondary w-100 mt-3"
+          >
+            Random Play
+          </button>
           <br />
-          <button onClick={this.handleReset} className="btn btn-warning w-100 mt-3">Reset</button>
+          <button
+            onClick={this.handleReset}
+            className="btn btn-warning w-100 mt-3"
+          >
+            Reset
+          </button>
         </div>
       </div>
     );
